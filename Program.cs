19dc448fb062d6c22f260d1cc5e8a1e47d11edf6
Joseph Cuevas <josephcuevas77@outlook.com>
2022-03-09 17:18:32 -0800

    C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program {
+    static void Main(string[] args){
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        
+        MyClass myClass = new MyClass();
+        
+    }
+
+    class MyClass{
+        
+        float myFloat; 
+        int myInt;
+
+        public MyClass(){
+            //Custom Constructor
+        }
+
+        void testFunction(){
+
+        }
+    }
+}
